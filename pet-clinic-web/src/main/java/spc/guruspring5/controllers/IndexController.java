bfb6f85f88429062aab5d4023c63fa0f74366dca
spc <teidei@live.com>
2020-08-23 12:28:31 +0100

    Add Pet Clinic Index Page and Controller, closes #8

---
+++ pet-clinic-web/src/main/java/spc/guruspring5/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package spc.guruspring5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * spc 23/08/20
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
