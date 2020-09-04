756d9c0d15b3d8e6d981f82e485e75f09f4b894f
spc <teidei@live.com>
2020-09-04 10:13:05 +0100

    List all Owners on Owner Index Page, closes #17

--- pet-clinic-web/src/main/java/spc/guruspring5/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/spc/guruspring5/controllers/OwnerController.java
@@ -1,7 +1,10 @@
 package spc.guruspring5.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import spc.guruspring5.services.OwnerService;
 
 /**
  * spc 27/08/20
@@ -10,8 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+    
+    /**
+     * @param ownerService
+     */
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner(){
+    public String listOwner(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
+
+    
 }
