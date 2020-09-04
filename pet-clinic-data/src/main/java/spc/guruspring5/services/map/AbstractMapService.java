99f7932092f0a2daeb9a4a571ce32dc04d1453ee
spc <teidei@live.com>
2020-09-04 17:48:06 +0100

    Update Map Impl to Manage Setting of ID Property, closes #24

--- pet-clinic-data/src/main/java/spc/guruspring5/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/spc/guruspring5/services/map/AbstractMapService.java
@@ -1,18 +1,24 @@
 package spc.guruspring5.services.map;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+
+import spc.guruspring5.model.BaseEntity;
+
 
 /**
  * spc 23/08/20
  * @param <T>
  * @param <ID>
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -22,8 +28,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){ // not sure what it return since t's generic
-         map.put(id, object);
+    T save(T object){ // not sure what it return since t's generic
+
+    if(object != null){
+       if(object.getId() == null){
+           object.setId(getNextId());
+       }
+       map.put(object.getId(), object);
+    } else{
+        throw new RuntimeException("Object Cannot be null");
+    }
         return object;
     }
 
@@ -33,5 +47,16 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object){
         map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+
+        Long nextId = null;
+        try{
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch(NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/spc/guruspring5/bootsrap/DataLoader.java
+++ pet-clinic-web/src/main/java/spc/guruspring5/bootsrap/DataLoader.java
@@ -28,14 +28,12 @@ public class DataLoader implements CommandLineRunner  {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Temi");
         owner1.setLastName("Lawrence");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Tumi");
         owner2.setLastName("Adewale");
 
@@ -45,14 +43,12 @@ public class DataLoader implements CommandLineRunner  {
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Mary");
         vet1.setLastName("Bila");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nisperb");
         vet2.setLastName("Ngisflourish");
 
