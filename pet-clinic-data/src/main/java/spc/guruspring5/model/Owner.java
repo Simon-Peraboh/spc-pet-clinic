bcac737e6c81f8ff26d9ccc83b589ced60a7ebfe
spc <teidei@live.com>
2020-09-07 16:15:40 +0100

    Create Pet Type, Pet and Visit Entities, closes #27

--- pet-clinic-data/src/main/java/spc/guruspring5/model/Owner.java
+++ pet-clinic-data/src/main/java/spc/guruspring5/model/Owner.java
@@ -1,6 +1,18 @@
 package spc.guruspring5.model;
+
+import java.util.Set;
+
 /**
  * spc 21/5/20
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
