d384d9c8b49e985d64ba2b6d96f777d16d578cb9
spc <teidei@live.com>
2020-09-07 16:23:49 +0100

    Create Vet Specialty Entities, Associate To Vet, closes #28

---
+++ pet-clinic-data/src/main/java/spc/guruspring5/model/Specialty.java
@@ -0,0 +1,16 @@
+package spc.guruspring5.model;
+/**
+ * spc 7/9/20
+ */
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/spc/guruspring5/model/Vet.java
+++ pet-clinic-data/src/main/java/spc/guruspring5/model/Vet.java
@@ -1,6 +1,19 @@
 package spc.guruspring5.model;
+
+import java.util.Set;
+
 /**
  * spc 21/5/20
  */
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
