b0846f3048653a90f53f1524a7eab6a81714e950
spc <teidei@live.com>
2020-09-17 14:40:54 +0100

    Create Visit Entities, closes #39

--- pet-clinic-data/src/main/java/spc/guruspring5/model/Visit.java
+++ pet-clinic-data/src/main/java/spc/guruspring5/model/Visit.java
@@ -1,14 +1,23 @@
 package spc.guruspring5.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * spc 7/9/20
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
