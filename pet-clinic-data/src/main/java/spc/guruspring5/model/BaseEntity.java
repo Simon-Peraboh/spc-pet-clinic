4a50a403db46904bcb4abb691e8e0f6eab280b73
spc <teidei@live.com>
2020-09-17 13:29:45 +0100

    Implement Base Entity (JPA), closes #36

--- pet-clinic-data/src/main/java/spc/guruspring5/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/spc/guruspring5/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package spc.guruspring5.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * spc 02-06-20
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
